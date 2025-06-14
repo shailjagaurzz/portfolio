4f51a6b065e2b40a01baa0216e2de6ba511c5eab
Shailja Gaur <gaurshailja8908@gmail.com>
2025-06-14 14:57:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Easter Egg: Click the title to hear Peter say "Shut up, Meg!"
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("h1").addEventListener("click", function() {
+        alert("Shut up, Meg!");
+    });
+});
+
+
